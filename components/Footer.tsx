6b0479614f23ae5adbf0db82ad6423c0d33fd917
Instituto FuturoOn de Tecnologia <desenvolvimento@institutofuturoon.org>
2025-11-09 11:39:17 -0300

    fix(footer, digital-literacy): Update contact number and add inscription modal
    Updates the footer contact phone number and introduces a new modal component for course inscriptions on the Digital Literacy view.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -75,7 +75,7 @@ const Footer: React.FC = () => {
             <ul className="mt-4 space-y-3">
                 <li><ContactInfo icon={<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" /></svg>}>Rua Silva Jardim, 689<br />Neves - São Gonçalo - RJ</ContactInfo></li>
                 <li><ContactInfo icon={<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" /><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" /></svg>}>futurooon@gmail.com</ContactInfo></li>
-                <li><ContactInfo icon={<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.518.758a10.024 10.024 0 004.486 4.486l.758-1.518a1 1 0 011.06-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" /></svg>}> (21) 97067-2194</ContactInfo></li>
+                <li><ContactInfo icon={<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.518.758a10.024 10.024 0 004.486 4.486l.758-1.518a1 1 0 011.06-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" /></svg>}> (21) 97087-2194</ContactInfo></li>
             </ul>
           </div>
         </div>
